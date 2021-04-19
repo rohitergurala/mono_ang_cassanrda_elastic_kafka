01988331d485a44f4c55bedd2205da7333e44750
rohit.ergurala <rohit.ergurala@emaratech.ae>
2021-04-19 17:59:18 +0400

    Initial Commit for monolith angular cassandra elastic kafka

---
+++ README-DOCKER-COMPOSE.md
@@ -0,0 +1,17 @@
+# JHipster generated Docker-Compose configuration
+
+## Usage
+
+Launch all your infrastructure by running: `docker-compose up -d`.
+
+## Configured Docker services
+
+### Applications and dependencies:
+
+- mono_ang_cass_elastic_kafka (monolith application)
+- mono_ang_cass_elastic_kafka's cassandra database
+
+### Additional Services:
+
+- Kafka
+- Zookeeper
---
+++ src/test/java/com/mycompany/myapp/cucumber/CucumberIT.java
@@ -0,0 +1,14 @@
+package com.mycompany.myapp.cucumber;
+
+import org.junit.runner.RunWith;
+
+import com.mycompany.myapp.AbstractCassandraTest;
+import io.cucumber.junit.CucumberOptions;
+import io.cucumber.junit.Cucumber;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(plugin = "pretty", features = "src/test/features")
+
+public class CucumberIT extends AbstractCassandraTest {
+
+}
